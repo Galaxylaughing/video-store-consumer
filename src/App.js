674962ab80129f44751c92ca35300ed35b508e45
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-12-16 14:16:42 -0800

    set up router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,67 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/search">Search</Link>
+            </li>
+            <li>
+              <Link to="/library">Library</Link>
+            </li>
+            <li>
+              <Link to="/customers">Customers</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/library">
+            <Library />
+          </Route>
+          <Route path="/customers">
+            <Customers />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     );
   }
+}
+
+function Home() {
+  return <h1>Homepage</h1>;
+}
+
+function Search() {
+  return <h1>Search for a Movie</h1>;
+}
+
+function Library() {
+  return <h1>Movie Library</h1>;
+}
+
+function Customers() {
+  return <h1>Customer List</h1>;
 }
 
 export default App;
