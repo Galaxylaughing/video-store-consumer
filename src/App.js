c9ac45f8e5b29cc8fe362fb955e968d06ae8a3cb
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-12-17 10:28:59 -0800

    added ability to checkout a movie, supplying movie title, customer id, and due date

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import CustomerList from './components/CustomerList';
 import MovieList from './components/MovieList';
 import Home from './components/Home';
 import MovieSearch from './components/MovieSearch';
+import { throwStatement } from '@babel/types';
 
 class App extends Component {
   constructor() {
@@ -24,6 +25,10 @@ class App extends Component {
       movies: [],
       selectedMovie: undefined,
       foundMovie: undefined,
+      checkoutResponse: {
+        checkoutSuccess: undefined,
+        checkoutError: undefined,
+      },
     }
   }
 
@@ -80,6 +85,51 @@ class App extends Component {
       });
   }
 
+  onCheckoutClick = () => {
+    const { selectedMovie, selectedCustomer } = this.state;
+    const movieTitle = selectedMovie.title;
+    const customerId = selectedCustomer.id;
+    console.log(`checking out ${movieTitle} to ${customerId}`);
+
+    // make checkoutDate
+    let currentDate = new Date()
+    const checkoutDate = currentDate.getDate();
+    const checkoutMonth = currentDate.getMonth() + 1; //months are 0-indexed in JS
+    const checkoutYear = currentDate.getFullYear();
+    console.log("today", checkoutDate, checkoutMonth, checkoutYear);
+
+    // make dueDate a week from now
+    // calculation future date from: https://stackoverflow.com/questions/1025693/how-to-get-next-week-date-in-javascript
+    const dueDate = new Date(currentDate.getTime() + 7 * 24 * 60 * 60 * 1000);
+    console.log("due date", dueDate);
+
+    // set up params for axios call
+    const checkoutParams = {
+      customer_id: customerId,
+      due_date: dueDate,
+    }
+    // make axios post request
+    axios.post(`http://localhost:3000/rentals/${movieTitle}/check-out`, checkoutParams)
+      .then((response) => {
+        console.log(response.data);
+        this.setState({
+          checkoutResponse: {
+            checkoutSuccess: response.data, // returns empty object
+            checkoutError: undefined,
+          }
+        })
+      })
+      .catch((error) => {
+        console.log(error.message);
+        this.setState({
+          checkoutResponse: {
+            checkoutSuccess: undefined,
+            checkoutError: error.message,
+          }
+        })
+      });
+  }
+
   render() {
     return (
       <Router>
@@ -125,6 +175,8 @@ class App extends Component {
             <Home 
               selectedCustomer={ this.state.selectedCustomer }
               selectedMovie={ this.state.selectedMovie }
+              onCheckoutClick={ this.onCheckoutClick }
+              checkoutResponse={ this.state.checkoutResponse }
             />
           </Route>
         </Switch>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Home.css';
 
-const Home = ({selectedCustomer, selectedMovie}) => {
+const Home = ({selectedCustomer, selectedMovie, onCheckoutClick, checkoutResponse}) => {
   return (
     <section>
       <h1>Homepage</h1>
-      <p>Selected Customer: { selectedCustomer ? selectedCustomer.name : "none" }</p>
-      <p>Selected Movie: { selectedMovie ? selectedMovie.title : "none" }</p>
+
+      <section>
+        { checkoutResponse.checkoutSuccess
+          ? <p className="checkout-success">Successfully checked out {selectedMovie.title} to {selectedCustomer.name}</p>
+          : ''
+        }
+
+        { checkoutResponse.checkoutError
+          ? <p className="checkout-error">Unable to checkout, encountered an error: {checkoutResponse.checkoutError}</p>
+          : ''
+        }
+      </section>
+
+      <section>
+        <p>Selected Customer: { selectedCustomer ? selectedCustomer.name : "none" }</p>
+        <p>Selected Movie: { selectedMovie ? selectedMovie.title : "none" }</p>
+
+        { (selectedCustomer && selectedMovie)
+          ? <button onClick={onCheckoutClick}>Checkout '{selectedMovie.title}' to {selectedCustomer.name}</button>
+          : '' 
+        }
+        
+      </section>
     </section>
   );
 }
@@ -14,6 +36,15 @@ const Home = ({selectedCustomer, selectedMovie}) => {
 Home.propTypes = {
   selectedCustomer: PropTypes.object,
   selectedMovie: PropTypes.object,
+  onCheckoutClick: PropTypes.func.isRequired,
+  checkoutResponse: PropTypes.object,
 }
 
 export default Home;
+
+// params[:customer_id]
+// params[:due_date]
+// params[:title]
+
+// http://localhost:3000/rentals/Jaws/check-out?customer_id=1&due_date=2019/12/18
+// http://localhost:3000/:title/check-out?customer_id=:customer_id&due_date=:due_date
